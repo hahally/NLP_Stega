a26aa5b1b66a3642723dd8c07d9a683181ff4dd6
hahally <47556859+hahally@users.noreply.github.com>
2022-09-19 10:31:46 +0800

    Update inference.py

--- steganalysis/TS_RNN/inference.py
+++ steganalysis/TS_RNN/inference.py
@@ -9,6 +9,34 @@ random.seed(202)
 np.random.seed(2022)
 tf.random.set_seed(2022)
 
+def test(test, 
+          tokens, 
+          folds=5, 
+          mode='rnn',
+          model_path='',
+          max_len=30, 
+          num_classes=2):
+    
+    word2indx = tokens.word_index
+    vocab_size = len(word2indx) + 1
+    
+    sentences = test.sentence.tolist()
+    x_test = tokens.texts_to_sequences(sentences)
+    x_test = tf.keras.preprocessing.sequence.pad_sequences(x_test, maxlen=max_len)
+    y_test = test.label.to_numpy()
+    
+    predictions = np.zeros([len(test), num_classes])
+    
+    for fold in range(1, folds+1):
+        bst_model_path = os.path.join(model_path, f"{mode}.{fold}.h5")
+        model = Tsrnn(mode=mode,vocab_size=vocab_size,max_len=max_len,num_classes=num_classes)
+        model.build(input_shape=(max_len,vocab_size))
+        model.load_weights(bst_model_path)
+        predictions += model.predict(x_test) / folds
+    
+    pre = np.argmax(predictions, axis = -1)
+    
+    return pre
 
 def infer(test, 
           tokens, 
