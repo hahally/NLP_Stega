5a78e5e105a925cdcedd26d22d53d82110f33c8f
hahally <47556859+hahally@users.noreply.github.com>
2022-11-22 23:51:07 +0800

    Update StegaCoding.py
    add SaBins

--- steganography/StegaCoding.py
+++ steganography/StegaCoding.py
@@ -41,3 +41,54 @@ def  ADG(voc_p, u=None):
     v = [x[0] for x in sorted_dict]
     group[f'group_{u-1}'] = v
     return group, u, k
+
+# 获取同义词
+from nltk.corpus import wordnet
+def get_synonyms(word):
+    synonyms = []
+    for syn in wordnet.synsets(word):
+        for lm in syn.lemmas():
+            syn_word = lm.name()
+            syn_word = syn_word.replace('_', ' ')
+            syn_word = syn_word.lower()
+            if syn_word==word:continue
+            if syn_word in synonyms:continue
+            synonyms.append(syn_word)
+    
+    return list(set(synonyms))
+# SaBins
+import random
+def SaBins(voc_prob, L=3, eos_token='<eos>'):
+    # 假设已经在语料库上进行了 词频统计 voc_prob 并且降序排列了
+    # voc_prob: [(w1,p1),...,]
+    voc = [vp[0] for vp in voc_prob]
+    
+    # 初始化分组 Initialize
+    num_groups = 2**L
+    bins = {str(i):set() for i in range(num_groups)}
+    bins[str(2**L)] = {eos_token}
+    if eos_token in voc:
+        voc.remove(eos_token)
+    # Mark all tokens in V as unprocessed
+    
+    for i in range(len(voc_prob)):
+        # Determine Cpi
+        C_pi = []
+        V_pi = voc_prob[i][0]
+        C_pi.append(V_pi)
+        C_pi += get_synonyms(word=V_pi)
+        # Collect all unprocessed tokens in Cpi
+        tmp_C_pi = list(filter(lambda x: x in voc, C_pi))
+        while len(tmp_C_pi)>0:
+            ns = min(len(tmp_C_pi), 2**L)
+            # Randomly select ns tokens
+            select_ns_tokens = random.sample(tmp_C_pi, k=ns)
+            # Randomly select ns subsets
+            select_ns_subsets = random.sample(range(num_groups), k=ns)
+            # Randomly assign the ns tokens to the ns subsets
+            for token, sub in zip(select_ns_tokens, select_ns_subsets):
+                bins[str(sub)].add(token)
+                voc.remove(token)
+                tmp_C_pi.remove(token)
+                
+    return bins
