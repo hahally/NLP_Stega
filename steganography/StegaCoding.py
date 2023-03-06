19b64519ab13e5364e4d479156f3004a6bfdf11d
hahally <47556859+hahally@users.noreply.github.com>
2023-03-06 12:02:28 +0800

    Huffman、FLC、Bins、ADG、Sabins

--- steganography/StegaCoding.py
+++ steganography/StegaCoding.py
@@ -1,5 +1,159 @@
 import math
 
+############################  Huffman  #####################################
+class Node:
+    def __init__(self,freq):
+        self.left = None
+        self.right = None
+        self.father = None
+        self.freq = freq
+    def isLeft(self):
+        return self.father.left == self
+#create nodes
+def createNodes(freqs):
+    return [Node(freq) for freq in freqs]
+
+#create Huffman Tree
+def createHuffmanTree(nodes):
+    queue = nodes[:]
+    while len(queue) > 1:
+        queue.sort(key=lambda item:item.freq)
+        node_left = queue.pop(0)
+        node_right = queue.pop(0)
+        node_father = Node(node_left.freq + node_right.freq)
+        node_father.left = node_left
+        node_father.right = node_right
+        node_left.father = node_father
+        node_right.father = node_father
+        queue.append(node_father)
+    queue[0].father = None
+    return queue[0]
+
+#Huffman encoding
+def huffmanEncoding(nodes,root):
+    codes = [''] * len(nodes)
+    for i in range(len(nodes)):
+        node_tmp = nodes[i]
+        while node_tmp != root:
+            if node_tmp.isLeft():
+                codes[i] = '0' + codes[i]
+            else:
+                codes[i] = '1' + codes[i]
+            node_tmp = node_tmp.father
+    return codes
+
+class Huffman():
+    def __init__(self,
+                 k=None,
+                 bits=None):
+        self.k = k
+        self.bits = bits
+        
+    def extract(self, pred_prob, word_index):
+        bit = None
+        prob, idx = torch.topk(pred_prob, k=self.k, dim=1)
+        word_prob = [[i.item(),j.item()] for i,j in zip(idx[0], prob[0])]
+        nodes = createNodes([item[1] for item in word_prob])
+        root = createHuffmanTree(nodes)
+        codes = huffmanEncoding(nodes, root)
+        for i, w_p in enumerate(word_prob):
+            if w_p[0] == word_index:
+                bit = codes[i]
+                break
+
+        return bit
+    
+    def __call__(self, pred_prob):
+        prob, idx = torch.topk(pred_prob, k=self.k, dim=1)
+        word_prob = [[i.item(),j.item()] for i,j in zip(idx[0], prob[0])]
+        nodes = createNodes([item[1] for item in word_prob])
+        root = createHuffmanTree(nodes)
+        codes = huffmanEncoding(nodes, root)
+        for i,code in enumerate(codes):
+            bit = self.bits[:len(code)]
+            if bit == code:
+                bit_word_index = word_prob[i][0]
+                self.bits = self.bits[len(code):]
+                break
+        if self.extract(pred_prob,bit_word_index)!=bit:
+            print("False")
+        bit_word_index = torch.LongTensor([bit_word_index]).to(pred_prob.device)
+        return bit_word_index
+
+############################  FLC  #####################################
+class FLC():
+    def __init__(self,
+                 k=None,
+                 bits=None):
+        self.k = k
+        self.bits = bits
+    
+    def extract(self, pred_prob, word_index):
+        bit = None
+        prob, idx = torch.topk(pred_prob, k=2**self.k, dim=1)
+        word_prob = [[i.item(),j.item()] for i,j in zip(idx[0], prob[0])]
+        codes = [str(bin(i))[2:].zfill(self.k) for i in range(2**self.k)]
+        for i, w_p in enumerate(word_prob):
+            if w_p[0] == word_index:
+                bit = codes[i]
+                break
+
+        return bit
+    
+    def __call__(self, pred_prob):
+        prob, idx = torch.topk(pred_prob, k=2**self.k, dim=1)
+        word_prob = [[i.item(),j.item()] for i,j in zip(idx[0], prob[0])]
+        codes = [str(bin(i))[2:].zfill(self.k) for i in range(2**self.k)]
+        bit = self.bits[:self.k]
+        for i,code in enumerate(codes):
+            if bit==code:
+                bit_word_index = word_prob[i][0]
+                self.bits = self.bits[self.k:]
+                break
+            
+        bit_word_index = torch.LongTensor([bit_word_index]).to(pred_prob.device)
+        return bit_word_index
+    
+############################   Bins   #####################################
+class Bins():
+    def __init__(self,
+                 bins=None,
+                 common_tokens=None,
+                 bits=None):
+        self.bins = bins
+        self.common_tokens = common_tokens
+        self.bits = bits
+        
+    def __call__(self, pred_prob):
+        """
+        input: 
+               pred_prob: 为模型预测概率分布, tensor
+               bit: 秘密信息,默认为已转为十进制的, int
+        output: 
+               state: 表示是否嵌入秘密信息, bool
+               bit_word_index: 嵌入秘密信息时的token, tensor
+        """
+        bit = self.bits[0]
+        state = True
+        targ = []
+        for i in range(len(self.bins)):
+            if i==bit:
+                targ += self.bins[i]
+                break
+            
+        mask = torch.zeros_like(pred_prob, device=pred_prob.device) - 1
+        mask[:,targ] = 0
+        pred_prob += mask
+        bit_word_index = torch.argmax(pred_prob, dim=1)
+        if int(bit_word_index[0].item()) in self.common_tokens:
+            state = False
+        
+        if state:
+            self.bits.pop(0)
+        
+        return state, bit_word_index
+
+############################   ADG   #####################################
 def  ADG(voc_p, u=None):
     sorted_dict= sorted(voc_p, key=lambda x: x[1], reverse = True) # [(w1,p1),...]
     v = [x[0] for x in sorted_dict]
@@ -56,7 +210,8 @@ def get_synonyms(word):
             synonyms.append(syn_word)
     
     return list(set(synonyms))
-# SaBins
+
+############################   SaBins   ######################################
 import random
 def SaBins(voc_prob, L=3, eos_token='<eos>'):
     # 假设已经在语料库上进行了 词频统计 voc_prob 并且降序排列了
